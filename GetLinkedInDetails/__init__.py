a77247ea1dbc465b7144065fc63c480a1299ea3b
Tomas Ye <tomaye@amazon.com>
2019-10-26 14:48:31 +0200

    Implementing the Mocked Linked Inn method

--- GetLinkedInDetails/__init__.py
+++ GetLinkedInDetails/__init__.py
@@ -1,24 +1,16 @@
 import logging
+import json
 
 import azure.functions as func
 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def main(req: func.HttpRequest) -> str:
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    logging.info('Get LinkedIn Data function has been called')
+    with open('./GetLinkedInDetails/TommyResponseJson.json') as json_file:
+        data = json.load(json_file)
+        logging.info('JSON = ')
+        logging.info(data)
+        return json.dumps(data)
+
