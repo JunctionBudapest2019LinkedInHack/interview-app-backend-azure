31994b9f6a527c7a57b12fddb6eea36c7f430107
Tomas Ye <tomaye@amazon.com>
2019-10-26 16:13:06 +0200

    The first GetDetail cloud function is able to successfully call the getLinkedInProfileInfo

--- GetAudioFromToken/__init__.py
+++ GetAudioFromToken/__init__.py
@@ -1,8 +1,17 @@
 import logging
 import azure.functions as func
+import requests
+
+getLinkedInDataUrl = 'https://junctionbudapest.azurewebsites.net/api/GetLinkedInDetails?code=UkMZMr9qcjxgvPgTF2bVViTfl8vdFqu5nRreUbjim0UwBR8x7df5HA=='
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Invoked Function for getting details ')
+    response = requests.get(getLinkedInDataUrl)
+    print('Response = ')
+    print(response.text)
+
+
+
     return func.HttpResponse(
         "Hello world",
         status_code=400
