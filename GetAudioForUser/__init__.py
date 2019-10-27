fc88fc5e426e7bb46c05090ec68bab1ecda540c5
Tomas Ye <tomaye@amazon.com>
2019-10-27 07:43:00 +0100

    Looks like it works locally!
    Damn I would be so happy...

--- GetAudioForUser/__init__.py
+++ GetAudioForUser/__init__.py
@@ -1,24 +1,39 @@
 import logging
-
 import azure.functions as func
+from azure.storage.blob import BlockBlobService
+import json
 
+
+output_container_name = 'junction-budapest-results'
+account_name = 'junctionbudapest2'
+account_key = '1TUratXtvByc86ruuQ8ptxw51GUwnF1DpSZP4oMipSbONChihpLGPpCbar6y1SWANds5Ch+AUVLKrsyxpj3xKg=='
+
+blobService = BlockBlobService(account_name=account_name, account_key=account_key)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
     name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    fileName = getFileName(name)
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+    return func.HttpResponse(
+             body=blobService.get_blob_to_text(output_container_name, fileName).content,
+             status_code=200
         )
+
+
+
+
+def getFileName(name):
+    mapping = {
+        'bernd': 'bernd_microsoft.json',
+        'hanka': 'hanka.json',
+        'lee': 'lee_microsoft.json',
+        'martin': 'martin_microsoft.json',
+        'lucie': 'lucie_microsoft.json',
+        'tommy': 'tommy.json',
+        'yuri': 'yuri.json'
+    }
+
+    if name in mapping.keys():
+        return mapping[name]
+    else:
+        raise Exception('Unknown linked in user ' + name)
